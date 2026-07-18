1a00cc24b6eddf7693c26d686f1ded9b854c0bb2
Никита Тимофеев <freemorobskyr@gmail.com>
2026-07-18 14:32:23 +0500

    fix: прокинул в пропс загрузки isSubmitting с формы у create-lesson-button

--- apps/frontend/src/features/create-lesson/ui/CreateLessonButton.tsx
+++ apps/frontend/src/features/create-lesson/ui/CreateLessonButton.tsx
@@ -4,6 +4,8 @@ import { Tooltip } from "@repo/ui/molecules/tooltip";
 import { CreateLessonForm } from "./CreateLessonForm";
 import { CreateLessonModal } from "./CreateLessonModal";
 import { useDisclosure } from "@repo/ui/hooks/use-disclosure";
+import type { CreateLessonFormData } from "../model/types";
+import { useFormContext } from "react-hook-form";
 
 export interface CreateLessonButtonProps {
   streamId: number;
@@ -15,11 +17,25 @@ export function CreateLessonButton({ streamId }: CreateLessonButtonProps) {
   return (
     <CreateLessonForm streamId={streamId} onSuccess={onClose}>
       <Tooltip text="Новое занятие" position="bottom_right">
-        <Button isIconOnly aria-label="Новое занятие" onClick={onOpen}>
-          <Icon name="plus" size={18} />
-        </Button>
+        <BaseCreateLessonButton onOpen={onOpen} />
       </Tooltip>
       <CreateLessonModal isOpen={isOpen} onClose={onClose} />
     </CreateLessonForm>
+  );
+}
+
+function BaseCreateLessonButton({ onOpen }: { onOpen: () => void }) {
+  const { formState } = useFormContext<CreateLessonFormData>();
+
+  return (
+    <Button
+      isIconOnly
+      isPending={formState.isSubmitting}
+      isDisabled={formState.isSubmitting}
+      aria-label="Новое занятие"
+      onClick={onOpen}
+    >
+      <Icon name="plus" size={18} />
+    </Button>
   );
 }
